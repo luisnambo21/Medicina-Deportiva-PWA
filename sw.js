07d5f459524ed1926c08a54e052d22da7f0cc4d8
luisnambo21 <arturo15villa@gmail.com>
2021-10-14 20:55:41 -0500

    primer commit del proyecto

---
+++ sw.js
@@ -0,0 +1,58 @@
+;
+
+const CACHE_NAME = 'v1_cache_Medicina',
+    urlsToCache = [
+        './',
+        'https://fonts.googleapis.com/css?family=Raleway:400,700',
+        'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
+        'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
+        'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
+        './style.css',
+        './script.js',
+        './img/saludilogo.png',
+        './img/saludilogo.png'
+    ]
+
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache)
+                .then(() => self.skipWaiting())
+        })
+        .catch(err => console.log('Falló registro de cache', err))
+    )
+})
+
+
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME]
+
+    e.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
+        })
+        .then(() => self.clients.claim())
+    )
+})
+
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request)
+        .then(res => {
+            if (res) {
+                return res
+            }
+            return fetch(e.request)
+        })
+    )
+})
